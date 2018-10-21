a741336f85054b4c6cf18a6fcdf2aa512067a330
Marcin <mboczkowski2@gmail.com>
2018-10-21 11:12:21 +0200

    V1.1 working generating PDF & adding validations

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const cityRes = document.getElementById("cityRes");
 const cashRes = document.getElementById("cashRes");
 const nickRes = document.getElementById("nickRes");
 const mailRes = document.getElementById("mailRes");
+const docNumberRes = document.getElementById("docNumberRes");
 const companyNameRes = document.getElementById("companyNameRes");
 const NIPRes = document.getElementById("NIPRes");
 const dateLabel = document.getElementById("date");
-let date = new Date();
-dateLabel.textContent =
-  date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+const companyCheckInput = document.getElementById("companyCheckInput");
+const errorsUl = document.querySelector(".errors ul");
+let errors = [];
+const date = new Date();
+dateLabel.textContent = `${date.getDate()}.${date.getMonth() +
+  1}.${date.getFullYear()}`;
 
 document.querySelector("button").addEventListener("click", () => {
+  errors = [];
   let number = document.getElementById("number").value;
   number = numberValid(number);
   const name = document.getElementById("name").value;
@@ -25,65 +30,176 @@ document.querySelector("button").addEventListener("click", () => {
   const city = document.getElementById("city").value;
   cityRes.textContent = city;
   const cash = document.getElementById("cash").value;
-  cashRes.textContent = cash;
+  cashValid(cash);
   const nick = document.getElementById("nick").value;
-  nickRes.textContent = nick;
+  nickValid(nick);
   const mail = document.getElementById("mail").value;
   mailRes.textContent = mail;
+  const docNumber = document.getElementById("docNumber").value;
+  docNumberRes.textContent = docNumber;
   const companyName = document.getElementById("companyName").value;
   companyNameRes.textContent = companyName;
   let nip = document.getElementById("NIP").value;
   nip = nipValid(nip);
+  errorsUl.textContent = "";
+  errorsLog();
+  createValid();
 });
-/////
-const companyCheckInput = document.getElementById("companyCheckInput");
 companyCheckInput.addEventListener("change", () => {
+  const changeVisible = (selector, style) => {
+    document.querySelector(selector).style.display = style;
+  };
   if (companyCheckInput.checked) {
-    document.querySelector(".companyName").style.display = "flex";
-    document.querySelector(".NIP").style.display = "flex";
+    changeVisible(".companyName", "flex");
+    changeVisible(".NIP", "flex");
   } else {
-    document.querySelector(".companyName").style.display = "none";
-    document.querySelector(".NIP").style.display = "none";
+    changeVisible(".companyName", "none");
+    changeVisible(".NIP", "none");
   }
 });
-///
+/// VALIDATING FUNCTIONS
 const numberValid = val => {
-  // console.logzz(val);
-  val = val.replace(/(\s)|(-)/g, "");
-  if (val.length != 26) {
-    alert("Niepoprawny numer konta");
+  if (val == "") {
+    errors.push("Proszę podać numer konta");
     return;
+  } else {
+    val = val.replace(/(\s)|(-)/g, "");
+    if (val.length != 26) {
+      errors.push("Proszę podać poprawny numer konta");
+      return;
+    }
+    let numberResArr = [];
+    numberResArr.push(val.substring(0, 2)); //first two numbers of bank number
+    let beginStr = 2;
+    let endStr = 6;
+    for (let i = 0; i < 7; i++) {
+      numberResArr.push(val.substring(beginStr, endStr));
+      beginStr += 4;
+      endStr += 4;
+    }
+    numberRes.textContent = `Numer konta: ${numberResArr.join(" ")}`;
   }
-  let numberResArr = [];
-  numberResArr.push(val.substring(0, 2)); //first two numbers of bank number
-  let beginStr = 2;
-  let endStr = 6;
-  for (let i = 0; i < 7; i++) {
-    numberResArr.push(val.substring(beginStr, endStr));
-    beginStr += 4;
-    endStr += 4;
-  }
-  numberRes.textContent = numberResArr.join(" ");
 };
+
+const cashValid = val => {
+  if (val == "") {
+    errors.push("Proszę podać kwotę zwrotu");
+    return;
+  } else {
+    cashRes.textContent = `Kwota zwrotu: ${val}`;
+  }
+};
+const nickValid = val => {
+  if (val == "") {
+    errors.push("Proszę podać nick");
+    return;
+  } else {
+    nickRes.textContent = `Nick: ${val}`;
+  }
+};
+
 const nipValid = val => {
-  val = val.replace(/(\s)|(-)/g, "");
-  if (val.length != 10) {
-    alert("Niepoprawny numer NIP");
-    return;
-  }
-  let NIPResArr = [];
-  NIPResArr.push(val.substring(0, 3));
-  NIPResArr.push(val.substring(3, 6));
-  NIPResArr.push(val.substring(6, 8));
-  NIPResArr.push(val.substring(8, 11));
+  if (companyCheckInput.checked) {
+    if (val == "") {
+      errors.push("Proszę podać NIP");
+      return;
+    } else {
+      val = val.replace(/(\s)|(-)/g, "");
+      if (val.length != 10) {
+        errors.push("Proszę podać poprawny numer NIP");
+        return;
+      }
+      let NIPResArr = [];
+      NIPResArr.push(val.substring(0, 3));
+      NIPResArr.push(val.substring(3, 6));
+      NIPResArr.push(val.substring(6, 8));
+      NIPResArr.push(val.substring(8, 11));
 
-  NIPRes.textContent = NIPResArr.join(" ");
+      NIPRes.textContent = `NIP: ${NIPResArr.join(" ")}`;
+    }
+  }
+};
+const errorsLog = () => {
+  errors.forEach(e => {
+    let li = document.createElement("li");
+    li.textContent = e;
+    errorsUl.appendChild(li);
+  });
+};
+const createValid = () => {
+  if (errors.length > 0) {
+    document.getElementById("generate").style.display = "none";
+  } else {
+    document.getElementById("generate").style.display = "flex";
+  }
 };
 
-// var doc = new jsPDF({
-//     unit: 'in',
-//     format: [4, 2]
-// })
+document.getElementById("generate").addEventListener("click", () => {
+  var docDefinition = {
+    content: [
+      {
+        text: `${numberRes.textContent}`,
+        style: "header"
+      },
+      {
+        text: `${adressRes.textContent}`,
+        style: "subheader"
+      },
+      {
+        text: `${postCodeRes.textContent}  ${cityRes.textContent}`,
+        style: "subheader"
+      },
+      {
+        text: `${nickRes.textContent}  ${mailRes.textContent}`,
+        style: "subheader"
+      },
+      {
+        text: `${companyNameRes.textContent}`,
+        style: "subheader"
+      },
+      {
+        text: `${NIPRes.textContent}`,
+        style: "subheader"
+      },
+      {
+        text: `${docNumberRes.textContent}`,
+        style: "subheaderMargin"
+      },
+      {
+        text: `${cashRes.textContent} zł`,
+        style: "red"
+      }
+    ],
 
-// doc.text('Hello world!', 1, 1)
-// doc.save('two-by-four.pdf')
+    footer: {
+      text: `${dateLabel.textContent}`,
+      style: "footer"
+    },
+
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 20, 0, 20]
+      },
+      subheader: {
+        fontSize: 15,
+        margin: [0, 5, 0, 5]
+      },
+      subheaderMargin: {
+        fontSize: 18,
+        margin: [0, 10, 0, 5],
+        bold: true
+      },
+      red: {
+        fontSize: 18,
+        color: "red",
+        bold: true
+      },
+      footer: {
+        margin: [10, 10, 10, 10]
+      }
+    }
+  };
+  pdfMake.createPdf(docDefinition).download("optionalName.pdf");
+});
